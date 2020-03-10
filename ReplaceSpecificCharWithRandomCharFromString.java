7074a6328fd540d3ab08c44174599aaf5fb6c8fe
Jacques Desire GRAHAM <jacques.graham17@gmail.com>
2020-03-10 01:52:49 +0100

    Update ReplaceSpecificCharWithRandomCharFromString.java

--- ReplaceSpecificCharWithRandomCharFromString.java
+++ ReplaceSpecificCharWithRandomCharFromString.java
@@ -11,6 +11,7 @@ public class  ReplaceSpecificCharWithRandomCharFromString {
     public static void main(String[] args) {
         System.out.println( replaceRandom1(input));
     }
+    //Replace all '#' in the string input with a random character from the string RANDOM_STRING
     private static String replaceRandom1(String inputString) {
     	return 	inputString.chars().mapToObj(c -> (char) c).map(x->  x=='#'    
     			          ?  RANDOM_STRING.charAt( new Random().nextInt(RANDOM_STRING.length()))
